8475b36ba503af11d063ee37e4c7713f17ca53f9
Jaewoo Kim <wodndhksk@gmail.com>
2022-04-28 13:20:59 +0900

    01-02 대소문자 변환

--- src/inflearn/javaAlgorithm/AlgorithmTest01_02.java
+++ src/inflearn/javaAlgorithm/AlgorithmTest01_02.java
@@ -1,9 +1,14 @@
 package inflearn.javaAlgorithm;
+/**
+ * 대문자와 소문자가 같이 존재하는 문자열을 입력받아 대문자는 소문자로
+ * 소문자는 대문자로 변환하여 출력하는 프로그램을 작성하세요.
+ */
 
 import java.util.Scanner;
 
 public class AlgorithmTest01_02 {
-    static String solution(String inputString){
+    //풀이
+    static String solution1(String inputString){
         char[] chars = inputString.toCharArray();
 
         for(int i =0; i< chars.length; i++){
@@ -16,10 +21,34 @@ public class AlgorithmTest01_02 {
 
         return str;
     }
+    //간단 풀이
+    String solution2(String str){
+        String answer = "";
+        for(char c : str.toCharArray()){
+            if(Character.isLowerCase(c))
+                answer += Character.toUpperCase(c);
+            else
+                answer += Character.toLowerCase(c);
+        }
+        return answer;
+    }
+
+    // 아스키값으로 풀이
+    String solution3(String str){
+        String answer = "";
+        for(char c : str.toCharArray()){
+            if(c>=97 && c<= 122)
+                answer += (char)(c-32);
+            else
+                answer += (char)(c+32);
+        }
+        return answer;
+    }
+
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         String inputString = sc.next();
 
-        System.out.println(solution(inputString));
+        System.out.println(solution1(inputString));
     }
 }

c6a5d4b9a6ade81b51a45ab5436bd358f663b0fd
Jaewoo Kim <wodndhksk@gmail.com>
2022-05-04 17:58:41 +0900

    01-07 회문문자열

---
+++ src/inflearn/javaAlgorithm/AlgorithmTest01_07.java
@@ -0,0 +1,29 @@
+package inflearn.javaAlgorithm;
+
+import java.util.Scanner;
+
+public class AlgorithmTest01_07 {
+
+    void solution1(String str){
+        String lowerStr = str.toLowerCase();
+        char[] chars = lowerStr.toCharArray();
+        int count = 0;
+        for(int i=0; i<chars.length/2; i++){
+            if(!(chars[i] == chars[(chars.length-i)-1]))
+                count++;
+        }
+        if(count ==0){
+            System.out.println("YES");
+        }else{
+            System.out.println("NO");
+        }
+    }
+
+    public static void main(String[] args) {
+        AlgorithmTest01_07 al = new AlgorithmTest01_07();
+        Scanner sc = new Scanner(System.in);
+        String inputString = sc.next();
+
+        al.solution1(inputString);
+    }
+}

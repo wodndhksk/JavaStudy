9b3b51890ae3a3175032210f2a3c72950915959b
Jaewoo Kim <wodndhksk@gmail.com>
2022-05-09 16:47:02 +0900

    01-10 가장 짧은 문자거리

---
+++ src/inflearn/javaAlgorithm/AlgorithmTest01_010.java
@@ -0,0 +1,61 @@
+package inflearn.javaAlgorithm;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class AlgorithmTest01_010 {
+
+    public String solution1(String str, char c){
+        String answer="";
+        int[] x = new int[str.length()];
+
+        int num = 101;
+        for(int i=0; i<str.length(); i++){
+            if(str.charAt(i) == c)
+                num = 0;
+            x[i] = num;
+            num++;
+        }
+        num = 101;
+        for(int i=str.length()-1; i >= 0; i--){
+            if(str.charAt(i) == c)
+                num = 0;
+            if(x[i] > num)
+                x[i] = num;
+            num++;
+        }
+        for(int i =0; i<x.length; i++)
+            answer += Integer.toString(x[i]) + " ";
+        return answer;
+    }
+    // 내 풀이
+    public String solution2(String str, char c){
+        String answer="";
+        char[] x = str.toCharArray();
+        ArrayList<Integer> charPos = new ArrayList<>();
+
+        for(int i=0; i<x.length; i++) {
+            int posNum = 0;
+            if (c == str.charAt(i)) charPos.add(i);
+        }
+        for(int i=0; i<x.length; i++){
+            int arr[] = new int[charPos.size()];
+            for(int j=0; j<charPos.size(); j++){
+                arr[j] = Math.abs(i - charPos.get(j));
+            }
+            Arrays.sort(arr);
+            answer += arr[0] + " ";
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        AlgorithmTest01_010 al = new AlgorithmTest01_010();
+        Scanner sc = new Scanner(System.in);
+        String inputString = sc.next();
+        char inputChar = sc.next().charAt(0);
+
+        System.out.println(al.solution1(inputString, inputChar).trim());
+    }
+}
